460aebeb2812494c910f2861afdc5e64d5f83e7c
Alejandro Pestana <alejandropestana@outlook.com>
2021-12-08 21:05:55 -0400

    Carpetas y Archivos Iniciales
    Creación de carpetas iniciales para organizar el proyecto. Inclusión del código de creación de tda, tablas, triggers y drops creados anteriormente en el Google Doc.

---
+++ scripts/creación/1-tda.sql
@@ -0,0 +1,221 @@
+CREATE OR REPLACE TYPE TDA_MANTENIMIENTO AS OBJECT (
+    fecha_prox_mant DATE,
+    STATIC FUNCTION fechaProxMant(fecha_ult_mant DATE DEFAULT CURRENT_DATE, num_meses INTEGER DEFAULT 1) RETURN DATE
+);
+
+/
+
+CREATE OR REPLACE TYPE BODY TDA_MANTENIMIENTO AS
+    STATIC FUNCTION fechaProxMant(fecha_ult_mant DATE DEFAULT CURRENT_DATE, num_meses INTEGER DEFAULT 1) RETURN DATE
+    IS
+    BEGIN
+        return ADD_MONTHS(fecha_ult_mant,num_meses);
+    END;
+END;
+
+/
+
+
+
+CREATE OR REPLACE TYPE DATOS AS OBJECT (
+    dat_tipo_cedula VARCHAR2(1),
+    dat_cedula NUMBER,
+    dat_primer_nombre VARCHAR2(40),
+    dat_segundo_nombre VARCHAR2(40),
+    dat_primer_apellido VARCHAR2(40),
+    dat_segundo_apellido VARCHAR2(40),
+    dat_genero VARCHAR2(1),
+    dat_fecha_nacimiento DATE,
+    dat_email VARCHAR2(320),
+    dat_vacunado NUMBER(1),
+
+    STATIC FUNCTION existeCedula (dat_tipo_cedula VARCHAR2, dat_cedula NUMBER) RETURN NUMBER,
+    STATIC FUNCTION validarCedula (dat_tipo_cedula VARCHAR2, dat_cedula NUMBER) RETURN NUMBER,
+    STATIC FUNCTION validarNombres (dat_primer_nombre VARCHAR2, dat_segundo_nombre VARCHAR2 DEFAULT NULL, dat_primer_apellido VARCHAR2, dat_segundo_apellido VARCHAR2 DEFAULT NULL) RETURN NUMBER,
+    STATIC FUNCTION validarGenero (dat_genero VARCHAR2) RETURN NUMBER,
+    STATIC FUNCTION existeEmail (dat_email VARCHAR2) RETURN NUMBER,
+    STATIC FUNCTION validarEmail (dat_email VARCHAR2) RETURN NUMBER,
+    STATIC FUNCTION validarVacunado (dat_vacunado NUMBER) RETURN NUMBER,
+    STATIC FUNCTION validarFechaNacimiento (dat_fecha_nacimiento DATE) RETURN NUMBER,
+    STATIC FUNCTION validarDatos (dat_tipo_cedula VARCHAR2, dat_cedula NUMBER, dat_primer_nombre VARCHAR2, dat_segundo_nombre VARCHAR2 DEFAULT NULL, dat_primer_apellido VARCHAR2, dat_segundo_apellido VARCHAR2 DEFAULT NULL, dat_genero VARCHAR2, dat_fecha_nacimiento DATE, dat_email VARCHAR2, dat_vacunado NUMBER) RETURN NUMBER,
+    STATIC FUNCTION obtenerEdad (fecha_nacimiento DATE) RETURN NUMBER,
+    STATIC FUNCTION obtenerEdad (cli_id NUMBER) RETURN NUMBER,
+    CONSTRUCTOR FUNCTION DATOS (dat_tipo_cedula VARCHAR2, dat_cedula NUMBER, dat_primer_nombre VARCHAR2, dat_segundo_nombre VARCHAR2 DEFAULT NULL, dat_primer_apellido VARCHAR2, dat_segundo_apellido VARCHAR2 DEFAULT NULL, dat_genero VARCHAR2, dat_fecha_nacimiento DATE, dat_email VARCHAR2, dat_vacunado NUMBER) RETURN SELF AS RESULT
+
+);
+
+/
+
+CREATE OR REPLACE TYPE BODY DATOS AS
+
+    STATIC FUNCTION existeCedula (dat_tipo_cedula VARCHAR2, dat_cedula NUMBER) RETURN NUMBER
+    IS
+        cedula NUMBER;
+    BEGIN
+        SELECT cl.cli_datos.dat_cedula 
+        INTO cedula
+        FROM CLIENTE cl
+        WHERE cl.cli_datos.dat_tipo_cedula = dat_tipo_cedula AND cl.cli_datos.dat_cedula = dat_cedula;
+        RETURN 1;
+        EXCEPTION
+            WHEN NO_DATA_FOUND THEN
+                RETURN 0;
+    END;
+
+
+    STATIC FUNCTION validarCedula (dat_tipo_cedula VARCHAR2, dat_cedula NUMBER) RETURN NUMBER
+    IS
+    BEGIN
+        IF (DATOS.existeCedula(dat_tipo_cedula, dat_cedula) = 1) THEN
+            RAISE_APPLICATION_ERROR(-20002,'Error. La cedula indicada ya existe en el sistema');
+            RETURN 0;
+        ELSIF (dat_tipo_cedula != 'V' AND dat_tipo_cedula != 'E') THEN
+            RAISE_APPLICATION_ERROR(-20003,'Error. Tipo de cedula invalido. Debe ser V o E');
+            RETURN 0;
+        ELSIF (dat_cedula >= 999999999 OR dat_cedula = 0) THEN
+            RAISE_APPLICATION_ERROR(-20004,'Error. Numero de cedula invalido');
+            RETURN 0;
+        ELSE
+            RETURN 1;
+        END IF;
+    END;
+
+    STATIC FUNCTION validarNombres (dat_primer_nombre VARCHAR2, dat_segundo_nombre VARCHAR2 DEFAULT NULL, dat_primer_apellido VARCHAR2, dat_segundo_apellido VARCHAR2 DEFAULT NULL) RETURN NUMBER
+    IS
+    BEGIN
+        IF (LENGTH(dat_primer_nombre) < 2 OR LENGTH(dat_primer_apellido) < 2) THEN
+            RAISE_APPLICATION_ERROR(-20005,'Error. Nombre o apellido muy corto');
+        END IF;
+        IF (LENGTH(TRIM(TRANSLATE(dat_primer_nombre, 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZáéíóúäëïöü', ' '))) IS NULL) THEN
+            IF (LENGTH(TRIM(TRANSLATE(dat_primer_nombre, 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZáéíóúäëïöü', ' '))) IS NULL) THEN
+                IF (LENGTH(TRIM(TRANSLATE(dat_primer_nombre, 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZáéíóúäëïöü', ' '))) IS NULL) THEN
+                    IF (LENGTH(TRIM(TRANSLATE(dat_primer_nombre, 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZáéíóúäëïöü', ' '))) IS NULL) THEN
+                        RETURN 1;
+                    END IF;
+                END IF;
+            END IF;
+        END IF;
+        RAISE_APPLICATION_ERROR(-20006,'Error. Los nombres y apellidos solo pueden contener letras');
+        RETURN 0;
+    END;
+    
+    STATIC FUNCTION validarGenero (dat_genero VARCHAR2) RETURN NUMBER
+    IS
+    BEGIN
+        IF (dat_genero != 'M' AND dat_genero != 'F') THEN
+            RAISE_APPLICATION_ERROR(-20007,'Error. Genero invalido');
+            RETURN 0;
+        ELSE
+            RETURN 1;
+        END IF;
+    END;
+    
+    STATIC FUNCTION existeEmail (dat_email VARCHAR2) RETURN NUMBER
+    IS
+        email VARCHAR2(320);
+    BEGIN
+        SELECT cl.cli_datos.dat_email
+        INTO email
+        FROM CLIENTE cl
+        WHERE cl.cli_datos.dat_email = dat_email;
+        RETURN 1;
+        EXCEPTION
+            WHEN NO_DATA_FOUND THEN
+                RETURN 0;
+    END;
+    
+    STATIC FUNCTION validarEmail (dat_email VARCHAR2) RETURN NUMBER
+    IS
+    BEGIN
+        IF(existeEmail(dat_email) = 1) THEN
+            RAISE_APPLICATION_ERROR(-20008,'Error. El email indicado ya existe en el sistema');
+            RETURN 0;
+        ELSIF(REGEXP_LIKE (dat_email, '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,4}$') AND LENGTH(dat_email) >= 3) THEN
+            RETURN 1;
+        ELSE
+            RAISE_APPLICATION_ERROR(-20009,'Error. Email invalido');
+            RETURN 0;
+        END IF;
+    END;
+    
+    STATIC FUNCTION validarVacunado (dat_vacunado NUMBER) RETURN NUMBER
+    IS
+    BEGIN
+        IF(dat_vacunado >= 0 AND dat_vacunado <= 1) THEN
+            RETURN 1;
+        ELSE
+            RAISE_APPLICATION_ERROR(-20010,'Error. Informacion de vacunacion invalida');
+            RETURN 0;
+        END IF;
+    END;
+    
+    STATIC FUNCTION validarFechaNacimiento (dat_fecha_nacimiento DATE) RETURN NUMBER
+    IS
+    BEGIN
+        IF((dat_fecha_nacimiento > SYSDATE) OR (floor(months_between(SYSDATE, dat_fecha_nacimiento) /12) >= 130)) THEN
+            RAISE_APPLICATION_ERROR(-20011,'Error. Fecha de nacimiento invalida');
+            RETURN 0;
+        ELSE
+            RETURN 1;
+        END IF;
+    END;
+    
+    STATIC FUNCTION validarDatos (dat_tipo_cedula VARCHAR2, dat_cedula NUMBER, dat_primer_nombre VARCHAR2, dat_segundo_nombre VARCHAR2, dat_primer_apellido VARCHAR2, dat_segundo_apellido VARCHAR2, dat_genero VARCHAR2, dat_fecha_nacimiento DATE, dat_email VARCHAR2, dat_vacunado NUMBER) RETURN NUMBER
+    IS
+    BEGIN
+        IF (DATOS.validarCedula(dat_tipo_cedula, dat_cedula) = 0) THEN
+            RETURN 0;
+        ELSIF (DATOS.validarNombres(dat_primer_nombre, dat_segundo_nombre, dat_primer_apellido, dat_segundo_apellido) = 0) THEN
+            RETURN 0;
+        ELSIF (DATOS.validarGenero(dat_genero) = 0) THEN
+            RETURN 0;
+        ELSIF (DATOS.validarEmail(dat_email) = 0) THEN
+            RETURN 0;
+        ELSIF (DATOS.validarVacunado(dat_vacunado) = 0) THEN
+            RETURN 0;
+        ELSIF (DATOS.validarFechaNacimiento(dat_fecha_nacimiento) = 0) THEN
+            RETURN 0;
+        ELSE
+            RETURN 1;
+        END IF;
+    END;
+
+    STATIC FUNCTION obtenerEdad (fecha_nacimiento DATE) RETURN NUMBER
+    IS
+    BEGIN
+        RETURN floor(months_between(SYSDATE, fecha_nacimiento) /12);
+    END;
+    
+    STATIC FUNCTION obtenerEdad (cli_id NUMBER) RETURN NUMBER
+    IS
+        fecha_nacimiento DATE;
+    BEGIN
+        SELECT cl.cli_datos.dat_fecha_nacimiento 
+        INTO fecha_nacimiento
+        FROM CLIENTE cl
+        WHERE cl.cli_id = 1;
+        RETURN floor(months_between(SYSDATE, fecha_nacimiento) /12);
+    END;
+
+    CONSTRUCTOR FUNCTION DATOS (dat_tipo_cedula VARCHAR2, dat_cedula NUMBER, dat_primer_nombre VARCHAR2, dat_segundo_nombre VARCHAR2 DEFAULT NULL, dat_primer_apellido VARCHAR2, dat_segundo_apellido VARCHAR2 DEFAULT NULL, dat_genero VARCHAR2, dat_fecha_nacimiento DATE, dat_email VARCHAR2, dat_vacunado NUMBER) RETURN SELF AS RESULT
+    IS
+    BEGIN
+        IF (DATOS.validarDatos(dat_tipo_cedula, dat_cedula, dat_primer_nombre, dat_segundo_nombre, dat_primer_apellido, dat_segundo_apellido, dat_genero, dat_fecha_nacimiento, dat_email, dat_vacunado) = 0) THEN
+            RAISE_APPLICATION_ERROR(-20012,'Error al crear cliente');
+        ELSE
+            SELF.dat_tipo_cedula := dat_tipo_cedula;
+            SELF.dat_cedula := dat_cedula;
+            SELF.dat_primer_nombre := INITCAP(dat_primer_nombre);
+            SELF.dat_segundo_nombre := INITCAP(dat_segundo_nombre);
+            SELF.dat_primer_apellido := INITCAP(dat_primer_apellido);
+            SELF.dat_segundo_apellido := INITCAP(dat_segundo_apellido);
+            SELF.dat_genero := dat_genero;
+            SELF.dat_fecha_nacimiento := dat_fecha_nacimiento;
+            SELF.dat_email := dat_email;
+            SELF.dat_vacunado := dat_vacunado;
+            RETURN;
+        END IF;
+    END;
+END;
+
+/
---
+++ scripts/creación/2-tablas.sql
@@ -0,0 +1,304 @@
+-- NIVEL 1 NIVEL 1 NIVEL 1 NIVEL 1 NIVEL 1 NIVEL 1 NIVEL 1 NIVEL 1 
+
+CREATE TABLE TRANSPORTE(
+    tra_id NUMBER PRIMARY KEY,
+    tra_foto BLOB NOT NULL,
+    tra_nombre VARCHAR2(40) NOT NULL
+);
+
+/
+
+
+
+CREATE TABLE CRUCERO(
+    tra_id NUMBER PRIMARY KEY,
+    cru_mant TDA_MANTENIMIENTO NOT NULL,
+    
+    CONSTRAINT FK_CRUCERO FOREIGN KEY (tra_id) REFERENCES TRANSPORTE(tra_id)
+);
+
+/
+
+
+
+CREATE TABLE TERRESTRE(
+    tra_id NUMBER PRIMARY KEY,
+    
+    CONSTRAINT FK_TERRESTRE FOREIGN KEY (tra_id) REFERENCES TRANSPORTE(tra_id)
+);
+
+/
+
+
+
+CREATE TABLE DESTINO_TURISTICO(
+    des_id NUMBER PRIMARY KEY,
+    des_nombre VARCHAR2(40) NOT NULL,
+    des_foto BLOB NOT NULL,
+    des_video BLOB NOT NULL,
+    des_descripcion VARCHAR2(200) NOT NULL
+);
+
+/
+
+
+
+CREATE TABLE PAIS (
+    pai_id NUMBER PRIMARY KEY NOT NULL,
+    pai_nombre VARCHAR2(40) NOT NULL,
+    pai_bandera BLOB NOT NULL
+);
+
+/
+
+
+
+CREATE TABLE EMPRESA (
+    emp_id NUMBER PRIMARY KEY NOT NULL,
+    emp_nombre VARCHAR2(40) NOT NULL,
+    emp_logo BLOB NOT NULL
+);
+
+/
+
+
+
+CREATE TABLE COMPETIDOR(
+    emp_id NUMBER PRIMARY KEY,
+    
+    CONSTRAINT fk_emp_id_comp FOREIGN KEY (emp_id) REFERENCES EMPRESA(emp_id)
+);
+
+/
+
+
+
+CREATE TABLE PROVEEDOR(
+    emp_id NUMBER PRIMARY KEY,
+    
+    CONSTRAINT fk_emp_id_prov FOREIGN KEY (emp_id) REFERENCES EMPRESA(emp_id)
+);
+
+/
+
+
+
+-- NIVEL 2 NIVEL 2 NIVEL 2 NIVEL 2 NIVEL 2 NIVEL 2 NIVEL 2 NIVEL 2 
+
+CREATE TABLE MANTENIMIENTO(
+    man_id NUMBER PRIMARY KEY,
+    man_fecha DATE NOT NULL,
+    man_descrip VARCHAR2(200) NOT NULL,
+    man_costo NUMBER NOT NULL,
+    tra_id NUMBER NOT NULL,
+    CONSTRAINT FK_MANTENIMIENTO FOREIGN KEY (tra_id) REFERENCES CRUCERO(tra_id)
+);
+
+/
+
+
+
+CREATE TABLE CLIENTE(
+    cli_id NUMBER PRIMARY KEY NOT NULL,
+    cli_datos DATOS NOT NULL,
+    pai_id NUMBER NOT NULL,
+    
+    CONSTRAINT fk_cliente_pais FOREIGN KEY (pai_id) REFERENCES PAIS (pai_id)
+);
+
+/
+
+
+
+CREATE TABLE VENTA(
+    ven_id NUMBER PRIMARY KEY NOT NULL,
+    ven_fecha DATE NOT NULL,
+    ven_cantidad NUMBER NOT NULL,
+    emp_id NUMBER NOT NULL,
+    
+    CONSTRAINT fk_emp_id_venta FOREIGN KEY (emp_id) REFERENCES COMPETIDOR(emp_id)
+);
+
+
+/
+
+
+
+CREATE TABLE ALIANZA(
+    ali_id NUMBER PRIMARY KEY NOT NULL,
+    ali_fecha FECHA,
+    emp_id NUMBER NOT NULL,
+    
+    CONSTRAINT fk_emp_id_ali FOREIGN KEY (emp_id) REFERENCES PROVEEDOR(emp_id)
+);
+
+/
+
+
+
+-- NIVEL 3 NIVEL 3 NIVEL 3 NIVEL 3 NIVEL 3 NIVEL 3 NIVEL 3 NIVEL 3 
+
+CREATE TABLE FAMILIAR(
+    cli_id1 NUMBER,
+    cli_id2 NUMBER,
+    
+    CONSTRAINT no_familiar_consigo_mismo CHECK (cli_id1 != cli_id2),
+    CONSTRAINT pk_familiar PRIMARY KEY (cli_id1, cli_id2),
+    CONSTRAINT fk_familiar_cliente1 FOREIGN KEY (cli_id1) REFERENCES CLIENTE (cli_id),
+    CONSTRAINT fk_familiar_cliente2 FOREIGN KEY (cli_id2) REFERENCES CLIENTE (cli_id)
+);
+
+/
+
+CREATE TABLE SERVICIO(
+    ser_id NUMBER PRIMARY KEY NOT NULL,
+    ser_nombre VARCHAR2(40) NOT NULL,
+    ser_foto BLOB NOT NULL,
+    ser_costo_mensual NUMBER NOT NULL,
+    ser_precio_unitario NUMBER NOT NULL,
+    ser_duracion NUMBER,
+    des_id NUMBER NOT NULL,
+    tra_id NUMBER,
+    ali_id NUMBER,
+    
+    CONSTRAINT fk_tra_id_servi FOREIGN KEY (tra_id) REFERENCES TRANSPORTE(tra_id),
+    CONSTRAINT fk_des_id_servi FOREIGN KEY (des_id) REFERENCES DESTINO_TURISTICO(des_id),
+    CONSTRAINT fk_ali_id_servi FOREIGN KEY (ali_id) REFERENCES ALIANZA(ali_id),
+    CONSTRAINT ser_cantidad_positiva CHECK (ser_precio_unitario >= 0 AND ser_costo_mensual >= 0)
+);
+
+/
+
+
+
+
+CREATE TABLE PCR(
+    pcr_id NUMBER PRIMARY KEY NOT NULL,
+    pcr_fecha DATE NOT NULL,
+    pcr_status NUMBER(1) NOT NULL,
+    cli_id NUMBER NOT NULL,
+    
+    CONSTRAINT fk_pcr_cliente FOREIGN KEY (cli_id) REFERENCES CLIENTE(cli_id)
+);
+
+/
+
+
+
+CREATE TABLE PAGO(
+    pag_id NUMBER PRIMARY KEY NOT NULL,
+    pag_canal VARCHAR2(40) NOT NULL,
+    pag_dispositivo VARCHAR2(40) NOT NULL,
+    pag_fecha DATE NOT NULL,
+    pag_monto_total NUMBER NOT NULL,
+    cli_id NUMBER NOT NULL,
+    
+    CONSTRAINT fk_cli_id_pago FOREIGN KEY (cli_id) REFERENCES CLIENTE(cli_id)
+);
+
+
+/
+
+
+
+-- NIVEL 4 NIVEL 4 NIVEL 4 NIVEL 4 NIVEL 4 NIVEL 4 NIVEL 4 NIVEL 4 
+
+CREATE TABLE DISPONIBILIDAD(
+    dis_id NUMBER PRIMARY KEY,
+    dis_fecha FECHA NOT NULL,
+    dis_cantidad_disp NUMBER NOT NULL,
+    ser_id NUMBER NOT NULL,
+    
+    CONSTRAINT fk_disponibilidad_servicio FOREIGN KEY (ser_id) REFERENCES SERVICIO (ser_id)
+);
+
+/
+
+
+
+CREATE TABLE OBSERVACION(
+    obs_id NUMBER PRIMARY KEY,
+    obs_calif NUMBER NOT NULL,
+    obs_texto VARCHAR(200),
+    obs_fecha DATE NOT NULL,
+    ser_id NUMBER NOT NULL,
+    cli_id NUMBER NOT NULL,
+    
+    CONSTRAINT intervalo_calificacion CHECK (obs_calif >= 0 AND obs_calif <= 3),
+    CONSTRAINT fk_observacion_servicio FOREIGN KEY (ser_id) REFERENCES SERVICIO (ser_id),
+    CONSTRAINT fk_observacion_cliente FOREIGN KEY (cli_id) REFERENCES CLIENTE (cli_id)
+);
+
+/
+
+
+
+CREATE TABLE PAQUETE_TURISTICO(
+    paq_id NUMBER PRIMARY KEY,
+    paq_precio PRECIO NOT NULL,
+    paq_fecha FECHA NOT NULL,
+    pag_id NUMBER NOT NULL,
+    des_id NUMBER NOT NULL,
+    
+    CONSTRAINT fk_paquete_pago FOREIGN KEY (pag_id) REFERENCES PAGO (pag_id),
+    CONSTRAINT fk_paquete_destino FOREIGN KEY (des_id) REFERENCES DESTINO_TURISTICO (des_id)
+  
+);
+
+/
+
+
+
+CREATE TABLE MEDIO_PAGO(
+    med_id NUMBER PRIMARY KEY NOT NULL,
+    med_nombre VARCHAR2(40) NOT NULL,
+    med_monto NUMBER NOT NULL,
+    pag_id NUMBER NOT NULL,
+    
+    CONSTRAINT fk_pag_id_med FOREIGN KEY (pag_id) REFERENCES PAGO(pag_id)
+);
+
+/
+
+
+
+CREATE TABLE ASISTENCIA(
+    asi_id NUMBER PRIMARY KEY NOT NULL,
+    asi_asistio NUMBER(1) DEFAULT 0 NOT NULL,
+    asi_fecha DATE,
+    cli_id NUMBER NOT NULL,
+    ser_id NUMBER NOT NULL,
+    
+    CONSTRAINT fk_cli_id_asi FOREIGN KEY (cli_id) REFERENCES CLIENTE(cli_id),
+    CONSTRAINT fk_ser_id_asi FOREIGN KEY (ser_id) REFERENCES SERVICICO(ser_id)
+);
+
+/
+
+
+
+-- NIVEL 5 NIVEL 5 NIVEL 5 NIVEL 5 NIVEL 5 NIVEL 5 NIVEL 5 NIVEL 5
+
+CREATE TABLE CARACTERISTICA(
+    paq_id NUMBER NOT NULL,
+    ser_id NUMBER NOT NULL,
+    car_fecha FECHA NOT NULL,
+    
+    CONSTRAINT pk_caracteristica PRIMARY KEY (paq_id, ser_id),
+    CONSTRAINT fk_paq_id_car FOREIGN KEY (paq_id) REFERENCES PAQUETE_TURISTICO(paq_id),
+    CONSTRAINT fk_ser_id_car FOREIGN KEY (ser_id) REFERENCES SERVICIO(ser_id)
+);
+
+/
+
+
+
+CREATE TABLE PERTENENCIA(
+    cli_id NUMBER NOT NULL,
+    paq_id NUMBER NOT NULL,
+    
+    CONSTRAINT fk_pert_cliente FOREIGN KEY (cli_id) REFERENCES CLIENTE(cli_id),
+    CONSTRAINT fk_pert_paquete FOREIGN KEY (paq_id) REFERENCES PAQUETE_TURISTICO(paq_id)
+);
+
+/
