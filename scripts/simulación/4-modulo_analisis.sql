bdde8ce3d1177813d67f92bd3da27510e5ea4f26
Alejandro Pestana <alejandropestana@outlook.com>
2021-12-15 01:37:42 -0400

    Cambiada cantidad de servicios a eliminar (ahora se crean 6 y se eliminan 6 por disponibilidad)

--- scripts/simulación/4-modulo_analisis.sql
+++ scripts/simulación/4-modulo_analisis.sql
@@ -26,6 +26,8 @@ CREATE OR REPLACE PACKAGE BODY MODULO_ANALISIS AS
         dbms_output.put_line(''); 
         dbms_output.put_line('Realizadas las compras y las observaciones de diferentes servicios, se procede a analizarlos:'); 
         dbms_output.put_line(''); 
+        dbms_output.put_line('Se agregarán 6 servicios y se eliminarán 6:'); 
+        dbms_output.put_line(''); 
     
         cont := 1; 
         -- 3 Servicios más demandados y disponibles actualmente 
@@ -136,7 +138,7 @@ CREATE OR REPLACE PACKAGE BODY MODULO_ANALISIS AS
         DBMS_OUTPUT.PUT_LINE(''); 
         cont := 1;     
         
-        -- 3 Servicios menos demandados y disponibles actualmente 
+        -- 2 Servicios menos demandados y disponibles actualmente 
         FOR servicio_menos_demandado IN  
             (SELECT se.ser_id, se.ser_nombre, de.des_nombre, sub.cantidad 
             FROM SERVICIO se 
@@ -150,7 +152,7 @@ CREATE OR REPLACE PACKAGE BODY MODULO_ANALISIS AS
             WHERE SYSDATE <= di.dis_fecha.fecha_fin 
             GROUP BY ca.ser_id 
             ORDER BY COUNT(ca.ser_id) ASC 
-            FETCH FIRST 3 ROWS ONLY) 
+            FETCH FIRST 2 ROWS ONLY) 
             sub ON sub.ser_id = se.ser_id)  
         LOOP 
             DBMS_OUTPUT.PUT_LINE('El servicio número '||cont||' menos demandado es '||servicio_menos_demandado.ser_nombre||' en '||servicio_menos_demandado.des_nombre); 
@@ -164,7 +166,7 @@ CREATE OR REPLACE PACKAGE BODY MODULO_ANALISIS AS
         DBMS_OUTPUT.PUT_LINE(''); 
         cont := 1;  
         
-        -- 3 Servicios peores calificados y disponibles actualmente 
+        -- 2 Servicios peores calificados y disponibles actualmente 
         FOR servicio_peor_calificado IN  
             (SELECT se.ser_nombre, de.des_nombre, sub.calificacion 
             FROM SERVICIO se 
@@ -178,7 +180,7 @@ CREATE OR REPLACE PACKAGE BODY MODULO_ANALISIS AS
             WHERE SYSDATE <= di.dis_fecha.fecha_fin 
             GROUP BY ob.ser_id 
             ORDER BY AVG(ob.obs_calif) ASC 
-            FETCH FIRST 3 ROWS ONLY) 
+            FETCH FIRST 2 ROWS ONLY) 
             sub ON sub.ser_id = se.ser_id)  
         LOOP 
             DBMS_OUTPUT.PUT_LINE('El servicio número '||cont||' peor calificado es'||servicio_peor_calificado.ser_nombre||' en '||servicio_peor_calificado.des_nombre); 
@@ -192,7 +194,7 @@ CREATE OR REPLACE PACKAGE BODY MODULO_ANALISIS AS
         DBMS_OUTPUT.PUT_LINE(''); 
         cont := 1;  
         
-        -- 3 Servicios con las peores relaciones ingreso vs egreso y disponibles actualmente 
+        -- 2 Servicios con las peores relaciones ingreso vs egreso y disponibles actualmente 
         FOR servicio_peor_ganancia IN  
             (SELECT se.ser_nombre, de.des_nombre, (sub.ganancia - se.ser_costo_mensual * ROUND(MONTHS_BETWEEN(fecha_ini,fecha_f)),0) relacionpc 
             FROM SERVICIO se 
@@ -208,7 +210,8 @@ CREATE OR REPLACE PACKAGE BODY MODULO_ANALISIS AS
             WHERE (SYSDATE <= di.dis_fecha.fecha_fin) AND (fecha_ini <= ca.car_fecha.fecha_inicio) AND (fecha_f >= ca.car_fecha.fecha_fin) 
             GROUP BY ca.ser_id) 
             sub ON sub.ser_id = se.ser_id 
-            ORDER BY relacionpc ASC) 
+            ORDER BY relacionpc ASC
+            FETCH FIRST 2 ROWS ONLY) 
         LOOP 
             DBMS_OUTPUT.PUT_LINE('El servicio número '||cont||' de peor relacion ganancia-costo es '||servicio_peor_ganancia.ser_nombre||' en '||servicio_peor_ganancia.des_nombre); 
             DBMS_OUTPUT.PUT_LINE('');
