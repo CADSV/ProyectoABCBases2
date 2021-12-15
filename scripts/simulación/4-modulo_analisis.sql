946b5ea6f9b963e7367f5a70586adc851f8f6e8f
Alejandro Pestana <alejandropestana@outlook.com>
2021-12-15 01:33:24 -0400

    Módulo análisis con inserts y updates

--- scripts/simulación/4-modulo_analisis.sql
+++ scripts/simulación/4-modulo_analisis.sql
@@ -8,7 +8,11 @@ END;
 CREATE OR REPLACE PACKAGE BODY MODULO_ANALISIS AS
     PROCEDURE analisis_servicios(fecha_ini DATE, fecha_f DATE)
     IS
-        cont NUMBER; 
+        cont NUMBER;
+        idali NUMBER;
+        idserv NUMBER; 
+        iddest NUMBER;
+        contloop NUMBER;
     BEGIN 
         DBMS_OUTPUT.PUT_LINE('----------------------------------------------------------------------------------'); 
         dbms_output.put_line(''); 
@@ -42,6 +46,34 @@ CREATE OR REPLACE PACKAGE BODY MODULO_ANALISIS AS
             sub ON sub.ser_id = se.ser_id)  
         LOOP 
             DBMS_OUTPUT.PUT_LINE('El servicio número '||cont||' más demandado es '||servicio_mas_demandado.ser_nombre||' en '||servicio_mas_demandado.des_nombre); 
+            dbms_output.put_line('');     
+            idali := ali_id_seq.nextVal;
+            INSERT INTO ALIANZA VALUES(idali, FECHA(fecha_f,fecha_f + 365*2), EMPTY_BLOB(), ROUND(dbms_random.value(16,30),0));
+            idserv := ser_id_seq.nextVal;
+            -- Buscar destino menos demandado
+            FOR destino_menos_demandado IN
+                (SELECT de.des_id, de.des_nombre
+                INTO iddest;
+                FROM SERVICIO se 
+                INNER JOIN DESTINO_TURISTICO de 
+                ON de.des_id = se.des_id 
+                INNER JOIN ( 
+                SELECT ca.ser_id, COUNT(ca.ser_id) 
+                FROM CARACTERISTICA ca 
+                INNER JOIN DISPONIBILIDAD di 
+                ON di.ser_id = ca.ser_id 
+                WHERE SYSDATE <= di.dis_fecha.fecha_fin 
+                GROUP BY ca.ser_id 
+                ORDER BY COUNT(ca.ser_id) ASC 
+                FETCH FIRST 1 ROWS ONLY) 
+                sub ON sub.ser_id = se.ser_id)
+            LOOP
+                dbms_output.put_line('Se concretó una alianza para suplir el servicio en el destino menos demandado: '||destino_menos_demandado.des_nombre);
+                dbms_output.put_line('');
+                iddest := destino_menos_demandado.des_id;
+            END LOOP;
+            INSERT INTO SERVICIO VALUES(idserv, servicio_mas_demandado.ser_nombre, ROUND(dbms_random.value(50,1000),0)), ROUND(dbms_random.value(1,250),0), 1, iddest, NULL, idali));
+            INSERT INTO DISPONIBILIDAD VALUES(dis_id_seq.nextVal, FECHA(fecha_f,fecha_f + 365*2), fecha_f, ROUND(dbms_random.value(50,1000),0)), idserv);
             cont := cont + 1; 
         END LOOP; 
         
@@ -66,6 +98,38 @@ CREATE OR REPLACE PACKAGE BODY MODULO_ANALISIS AS
             sub ON sub.ser_id = se.ser_id)  
         LOOP 
             DBMS_OUTPUT.PUT_LINE('El servicio número '||cont||' mejor calificado es '||servicio_mejor_calificado.ser_nombre||' en '||servicio_mejor_calificado.des_nombre); 
+            dbms_output.put_line(''); 
+            idali := ali_id_seq.nextVal;
+            INSERT INTO ALIANZA VALUES(idali, FECHA(fecha_f,fecha_f + 365*2), EMPTY_BLOB(), ROUND(dbms_random.value(16,30),0));
+            idserv := ser_id_seq.nextVal;
+            contloop := 1;
+            -- Buscar segundo destino menos demandado
+            FOR seg_destino_menos_demandado IN
+                (SELECT de.des_id, de.des_nombre
+                INTO iddest;
+                FROM SERVICIO se 
+                INNER JOIN DESTINO_TURISTICO de 
+                ON de.des_id = se.des_id 
+                INNER JOIN ( 
+                SELECT ca.ser_id, COUNT(ca.ser_id) 
+                FROM CARACTERISTICA ca 
+                INNER JOIN DISPONIBILIDAD di 
+                ON di.ser_id = ca.ser_id 
+                WHERE SYSDATE <= di.dis_fecha.fecha_fin 
+                GROUP BY ca.ser_id 
+                ORDER BY COUNT(ca.ser_id) ASC 
+                FETCH FIRST 2 ROWS ONLY) 
+                sub ON sub.ser_id = se.ser_id)
+            LOOP
+                IF(contloop = 2) THEN
+                    dbms_output.put_line('Se concretó una alianza para suplir el servicio en el segundo destino menos demandado: '||seg_destino_menos_demandado.des_nombre);
+                    dbms_output.put_line('');
+                    iddest := seg_destino_menos_demandado.des_id;
+                END IF;
+                contloop := contloop + 1;
+            END LOOP;
+            INSERT INTO SERVICIO VALUES(idserv, servicio_mas_demandado.ser_nombre, ROUND(dbms_random.value(50,1000),0)), ROUND(dbms_random.value(1,250),0), 1, iddest, NULL, idali));
+            INSERT INTO DISPONIBILIDAD VALUES(dis_id_seq.nextVal, FECHA(fecha_f,fecha_f + 365*2), fecha_f, ROUND(dbms_random.value(50,1000),0)), idserv);         
             cont := cont + 1; 
         END LOOP; 
         
@@ -74,7 +138,7 @@ CREATE OR REPLACE PACKAGE BODY MODULO_ANALISIS AS
         
         -- 3 Servicios menos demandados y disponibles actualmente 
         FOR servicio_menos_demandado IN  
-            (SELECT se.ser_nombre, de.des_nombre, sub.cantidad 
+            (SELECT se.ser_id, se.ser_nombre, de.des_nombre, sub.cantidad 
             FROM SERVICIO se 
             INNER JOIN DESTINO_TURISTICO de 
             ON de.des_id = se.des_id 
@@ -90,6 +154,10 @@ CREATE OR REPLACE PACKAGE BODY MODULO_ANALISIS AS
             sub ON sub.ser_id = se.ser_id)  
         LOOP 
             DBMS_OUTPUT.PUT_LINE('El servicio número '||cont||' menos demandado es '||servicio_menos_demandado.ser_nombre||' en '||servicio_menos_demandado.des_nombre); 
+            DBMS_OUTPUT.PUT_LINE('');
+            UPDATE DISPONIBILIDAD SET dis_fecha.fecha_fin = SYSDATE WHERE ser_id = servicio_menos_demandado.ser_id;
+            DBMS_OUTPUT.PUT_LINE('Se descontinuó el servicio.');
+            DBMS_OUTPUT.PUT_LINE('');
             cont := cont + 1; 
         END LOOP; 
         
@@ -114,6 +182,10 @@ CREATE OR REPLACE PACKAGE BODY MODULO_ANALISIS AS
             sub ON sub.ser_id = se.ser_id)  
         LOOP 
             DBMS_OUTPUT.PUT_LINE('El servicio número '||cont||' peor calificado es'||servicio_peor_calificado.ser_nombre||' en '||servicio_peor_calificado.des_nombre); 
+            DBMS_OUTPUT.PUT_LINE('');
+            UPDATE DISPONIBILIDAD SET dis_fecha.fecha_fin = SYSDATE WHERE ser_id = sservicio_peor_calificado.ser_id;
+            DBMS_OUTPUT.PUT_LINE('Se descontinuó el servicio.');
+            DBMS_OUTPUT.PUT_LINE('');            
             cont := cont + 1; 
         END LOOP; 
 
@@ -139,7 +211,17 @@ CREATE OR REPLACE PACKAGE BODY MODULO_ANALISIS AS
             ORDER BY relacionpc ASC) 
         LOOP 
             DBMS_OUTPUT.PUT_LINE('El servicio número '||cont||' de peor relacion ganancia-costo es '||servicio_peor_ganancia.ser_nombre||' en '||servicio_peor_ganancia.des_nombre); 
+            DBMS_OUTPUT.PUT_LINE('');
+            UPDATE DISPONIBILIDAD SET dis_fecha.fecha_fin = SYSDATE WHERE ser_id = servicio_peor_ganancia.ser_id;
+            DBMS_OUTPUT.PUT_LINE('Se descontinuó el servicio.');
+            DBMS_OUTPUT.PUT_LINE('');
             cont := cont + 1; 
         END LOOP;
+
+        UPDATE DISPONIBILIDAD SET dis_fecha.fecha_fin = (dis_fecha.fecha_fin + 365) WHERE dis_fecha.fecha_fin > SYSDATE;
+        DBMS_OUTPUT.PUT_LINE('Se renovaron las disponibilidades de todos los demás servicios por 1 año.');
+        DBMS_OUTPUT.PUT_LINE('');
+        DBMS_OUTPUT.PUT_LINE('Fin de la simulación.');
+        DBMS_OUTPUT.PUT_LINE('----------------------------------------------------------------------------------'); 
     END;
 END;
